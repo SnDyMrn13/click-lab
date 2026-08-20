67fdfb37b8b250013d8edc90ca315feef5b2bcab
Cindy <cindino45@gmail.com>
2026-08-20 15:59:14 -0500

    add a line that answers back, closes#3

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ let clueButton=document.getElementById("clue-button");
 let monopolyButton=document.getElementById("monopoly-button");
 let scrabbleButton=document.getElementById("scrabble-button");
 let answerText=document.getElementById("answer-text");
+let pickOne=document.getElementById("pick-one");
 
 
 clueButton.addEventListener("click", function(){
 
 answerText.textContent="Nope!";
 answerText.style.color="red";
+pickOne.textContent="";
 });
 
 
@@ -17,6 +19,7 @@ monopolyButton.addEventListener("click", function(){
 
 answerText.textContent="Correct!";
 answerText.style.color="green";
+pickOne.textContent="In 1903, Lizzie Magie invented The 'Landlord's game'.  It would later (with a few adjustments) become known as 'Monopoly'.";
 	
 });
 
@@ -25,6 +28,7 @@ scrabbleButton.addEventListener("click", function(){
 	
 answerText.textContent="Nope!";
 answerText.style.color="red";
+pickOne.textContent="";
 
 });
 
