3488af289c7e06a359344ed906fbb3e904d72ccd
Cindy <cindino45@gmail.com>
2026-08-21 21:22:36 -0500

    added scoreboard.css

--- script.js
+++ script.js
@@ -3,88 +3,105 @@ let headline=document.getElementById("headline-1");
 let clueButton=document.getElementById("clue-button");
 let monopolyButton=document.getElementById("monopoly-button");
 let scrabbleButton=document.getElementById("scrabble-button");
-let answerText=document.getElementById("answer-text");
-let pickOne=document.getElementById("pick-one");
-let trivialPursuitButton=document.getElementById("trivialPursuit-button");
+let answerText1=document.getElementById("answer-text1");
+let pickOne1=document.getElementById("pick-one1");
+let trivialPursuitButton=document.getElementById("trivialpursuit-button");
 let monopoly2Button=document.getElementById("monopoly2-button");
 let scrabble2Button=document.getElementById("scrabble2-button");
-let mouseTrapButton=document.getElementById("mouseTrap-button");
+let mouseTrapButton=document.getElementById("mousetrap-button");
 let troubleButton=document.getElementById("trouble-button");
 let candylandButton=document.getElementById("candyland-button");
+let answerText2=document.getElementById("answer-text2");
+let pickOne2=document.getElementById("pick-one2");
+let answerText3=document.getElementById("answer-text3");
+let pickOne3=document.getElementById("pick-one3");
+let scoreText=document.getElementById("score-1");
+let score=0;
+
+
 
 
 
 
 clueButton.addEventListener("click", function(){
-
-	answerText.textContent="Nope!";
-	answerText.style.color="red";
-	pickOne.textContent="";
+	score=score - 1;
+	scoreText.textContent=score;
+	answerText1.textContent="Nope!";
+	answerText1.style.color="red";
+	pickOne1.textContent="";
 });
 
 
 monopolyButton.addEventListener("click", function(){
-
-	answerText.textContent="Correct!";
-	answerText.style.color="green";
-	pickOne.textContent="In 1903, Lizzie Magie invented The 'Landlord's game'.  It would later (with a few adjustments) become known as 'Monopoly'.";
+	score=score + 1;
+	scoreText.textContent=score;
+	answerText1.textContent="Correct!";
+	answerText1.style.color="green";
+	pickOne1.textContent="In 1903, Lizzie Magie invented The 'Landlord's game'.  It would later (with a few adjustments) become known as 'Monopoly'.";
 	
 });
 
 
 scrabbleButton.addEventListener("click", function(){
-	
-	answerText.textContent="Nope!";
-	answerText.style.color="red";
-	pickOne.textContent="";
+	score=score - 1;
+	scoreText.textContent=score;
+	answerText1.textContent="Nope!";
+	answerText1.style.color="red";
+	pickOne1.textContent="";
 
 });
 
 trivialPursuitButton.addEventListener("click", function(){
-	
-	answerText.textContent="Nope!";
-	answerText.style.color="red";
-	pickOne.textContent="";
+	score=score - 1;
+	scoreText.textContent=score;
+	answerText2.textContent="Nope!";
+	answerText2.style.color="red";
+	pickOne2.textContent="";
 
 });
 
 monopoly2Button.addEventListener("click", function(){
-	
-	answerText.textContent="Correct!";
-	answerText.style.color="green";
-	pickOne.textContent="Monopoly has sold over 275 million copies worldwide.";
+	score=score + 1;
+	scoreText.textContent=score;
+	answerText2.textContent="Correct!";
+	answerText2.style.color="green";
+	pickOne2.textContent="Monopoly has sold over 275 million copies worldwide.";
 
 });
 
 scrabble2Button.addEventListener("click", function(){
-
-	answerText.textContent="Nope!";
-	answerText.style.color="red";
-	pickOne.textContent="";
+	score=score - 1;
+	scoreText.textContent=score;
+	answerText2.textContent="Nope!";
+	answerText2.style.color="red";
+	pickOne2.textContent="";
 	
 });
 
-mouseTrapButton.addEventListener("click", function();
-
-	answerText.textContent="Nope!";
-	answerText.style.color="red";
-	pickOne.textContent="";
+mouseTrapButton.addEventListener("click", function(){
+	score=score - 1;
+	scoreText.textContent=score;
+	answerText3.textContent="Nope!";
+	answerText3.style.color="red";
+	pickOne3.textContent="";
 	
 });
 
 candylandButton.addEventListener("click", function(){
-	
-	answerText.textContent="Correct!";
-	answerText.style.color="green";
-	pickOne.textContent="In 1948, Eleanor Abbott invented Candyland while she was recovering from Polio in a San Diego hospital.";
+	score= score + 1;
+	scoreText.textContent=score;
+	answerText3.textContent="Correct!";
+	answerText3.style.color="green";
+	pickOne3.textContent="In 1948, Eleanor Abbott invented Candyland while she was recovering from Polio in a San Diego hospital.";
 	
 });
 
 troubleButton.addEventListener("click", function(){
-	
-	answerText.textContent="Nope!";
-	answerText.style.color="red";
-	pickOne.textContent="";
+	score=score -1;
+	scoreText.textContent=score;
+	answerText3.textContent="Nope!";
+	answerText3.style.color="red";
+	pickOne3.textContent="";
 	
 });
 
